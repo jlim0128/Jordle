29d9f200cc13d03da8bdfceb89d8b1aa75e88079
jlim0128 <103789061+jlim0128@users.noreply.github.com>
2022-05-25 16:59:37 -0400

    Update Words.java

--- Words.java
+++ Words.java
@@ -7,11 +7,4 @@ public class Words {
         "inset", "logic", "mouse", "nodes", "pixel", "print", "scope", "short", "stack", "stage", "super", "throw",
         "token", "value", "while", "world", "write"
     ));
-
-    // use this ArrayList if you want to attempt one of the extra credit options (accounting for duplicate characters)
-    
-   /*  public static ArrayList<String> list = new ArrayList<>(Arrays.asList(
-        "array", "catch", "class","error", "event", "hello", "inner", "javac", "merge", "nlogn", "queue", "scene"
-    ));    
-     */
 }
